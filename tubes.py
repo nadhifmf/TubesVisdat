8f465ce17677e8c56cdc22e43a79e273da29bc11
nadhifmf <nadhifmmff@gmail.com>
2022-01-09 18:26:41 +0700

    master

--- tubes.py
+++ tubes.py
@@ -14,7 +14,7 @@ from bokeh.models import DatetimeTickFormatter, ColumnDataSource
 output_notebook()
 
 #loading dataset
-data = pd.read_excel('./data/kasus aktif cov 19 jawa bali.xlsx')
+data = pd.read_csv('./data/kasus aktif cov 19 jawa bali.csv')
 
 #change "tanggal" column tp datetime format from string
 data['Tanggal'] =  pd.to_datetime(data['Tanggal'], format='%y-%m-%d')
